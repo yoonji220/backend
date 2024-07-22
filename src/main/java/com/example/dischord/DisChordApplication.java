d4ff1b3e05218d9e8192502b1bb39bcae8399150
7zrv <4014133@gmail.com>
2024-07-22 18:05:33 +0900

    Initial commit

---
+++ src/main/java/com/example/dischord/DisChordApplication.java
@@ -0,0 +1,13 @@
+package com.example.dischord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisChordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisChordApplication.class, args);
+    }
+
+}
